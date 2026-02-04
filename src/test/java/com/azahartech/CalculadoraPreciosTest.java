0ac9278545ef81f214da2fcb5020d7d04e95da29
Lary David <larydavid34@gmail.com>
2026-02-04 20:21:36 +0100

    Tercer commit: hemos cambiado el assertEquals de verificaCalculosCorrectos para ver como falla

--- src/test/java/com/azahartech/CalculadoraPreciosTest.java
+++ src/test/java/com/azahartech/CalculadoraPreciosTest.java
@@ -8,7 +8,7 @@ class CalculadoraPreciosTest {
         CalculadoraPrecios cal = new CalculadoraPrecios();
         // 100€, 2 entradas, Estudiante (1) -> 200€ total. >100 aplica 10%.
         // 200 - 20 = 180. + 21% IVA = 217.8
-        assertEquals(217.8, cal.calcularPrecioTotal(100, 2, 1), 0.01);
+        assertEquals(218.8, cal.calcularPrecioTotal(100, 2, 1), 0.01);
         // 100€, 1 entrada, VIP (2) -> 100€.
         // 100 + 50 (VIP) = 150. + 21% IVA = 181.5
         assertEquals(181.5, cal.calcularPrecioTotal(100, 1, 2), 0.01);
